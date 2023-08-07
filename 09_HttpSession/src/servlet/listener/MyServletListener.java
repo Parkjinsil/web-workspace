476113d401f18b2574fdb289a92761a674879873
박진실 <wlstlf971017@naver.com>
2023-08-07 17:41:54 +0900

    0807feat

---
+++ 09_HttpSession/src/servlet/listener/MyServletListener.java
@@ -0,0 +1,19 @@
+package servlet.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class MyServletListener implements ServletContextListener {
+
+    public void contextDestroyed(ServletContextEvent sce)  { 
+         System.out.println("contextDestroyed...");
+    }
+
+    // dd(web.xml) 읽고 - ServletContext 생성 - ServletContextEvent 발생 - 리스너 감지 - 호출
+    public void contextInitialized(ServletContextEvent sce)  { 
+         System.out.println("contextInitialized...");
+         
+         // sce에서 ServletContext를 받아온다.
+    }
+	
+}
